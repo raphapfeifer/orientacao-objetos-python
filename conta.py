d4008506a67199dd227eb5db73c2d91a49d184ca
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-11-15 22:58:20 -0300

    criaçáo da classe conta

--- conta.py
+++ conta.py
@@ -1,4 +1,10 @@
 
 class Conta:
 
-    pass
+    def __init__(self,numero,titular,saldo,limite):
+        print("Construindo objeto... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
